8110036c1209cdaf1e3e12e14bde289394e112f3
Graham Daker <m211332@usna.edu>
2021-03-04 14:41:04 -0500

    debug

--- pics.py
+++ pics.py
@@ -4,6 +4,7 @@ r = robot()
 while True:
     angle = float(input("Angle: "))
     while ( abs(r.getAngle() - angle) > .05 ):
+        print(r.getAngle())
         r.drive(angularSpeed=pid_speed(-.1, 0, 0, angleDiff(r.getAngle()[2], angle)))
     img = r.getImage()
     img.save('image_' + str(angle))
