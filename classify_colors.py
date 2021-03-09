7a2a597ecf96274218e4d4fd0dae9df8e19fc187
Administrator <administrator@kinetic.kinetic>
2021-03-09 14:50:25 -0500

    recognizes red

--- classify_colors.py
+++ classify_colors.py
@@ -4,5 +4,5 @@ import numpy as np
 # red
 img = cv2.imread('red.jpg')
 hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-outhsv = cv2.inRange(hsv,np.array([358,60,20]),np.array([0,255,235]))
+outhsv = cv2.inRange(hsv,np.array([-3,15,15]),np.array([5,255,255]))
 cv2.imwrite('kermit2.jpg',outhsv)
