c5f16538128889863d77b240fe8d28645ab62cab
Administrator <administrator@kinetic.kinetic>
2021-03-09 15:04:46 -0500

    pink

--- seePink.py
+++ seePink.py
@@ -2,5 +2,5 @@ import cv2
 import numpy as np
 img = cv2.imread('pink.jpg')
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-outhsv = cv2.inRange(hsv, np.array([15, 35, 20]), np.array([255, 80, 235]))
+outhsv = cv2.inRange(hsv, np.array([304, 15, 15]), np.array([309,255,255]))
 cv2.imwrite('pink2.jpg', outhsv)
