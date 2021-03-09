9ca198abe378031020c7ef77232948bd0ec81689
cgdaker <57673795+cgdaker@users.noreply.github.com>
2021-03-09 14:59:06 -0500

    te

--- classify_colors.py
+++ classify_colors.py
@@ -4,8 +4,8 @@ import numpy as np
 # constants
 red_lower = np.array([-3,15,15])
 red_upper = np.array([5,255,255])
-blue_lower = np.array([325, 15, 15])
-blue_upper = np.array([335, 255, 255])
+blue_lower = np.array([230, 15, 15])
+blue_upper = np.array([250, 255, 255])
 
 # red
 img = cv2.imread('pink.jpg')
