30d4db6a1f7e69bf505b33ce3c7e2f4f8e808ad0
cgdaker <57673795+cgdaker@users.noreply.github.com>
2021-03-11 19:41:37 -0500

    f

--- color/classify_colors.py
+++ color/classify_colors.py
@@ -8,11 +8,11 @@ pink_lower = np.array([295, 15, 15])
 pink_upper = np.array([310, 100, 100])
 green_lower= np.array([55, 15, 15])
 green_upper= np.array([65, 255, 255])
-blue_lower = np.array([115, 15, 15])
-blue_upper = np.array([125, 255, 255])
+blue_lower = np.array([115, 15, 15])     # done
+blue_upper = np.array([125, 255, 255])   # done
 
 # red
-img = cv2.imread('blue.jpg')
+img = cv2.imread('green.jpg')
 hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-outhsv = cv2.inRange(hsv,blue_lower,blue_upper)
+outhsv = cv2.inRange(hsv,green_lower,green_upper)
 cv2.imwrite('kermit2.jpg',outhsv)
