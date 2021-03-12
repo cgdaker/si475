89803d81b0e152ff0bf3170d0c3ff66535d2c5a6
cgdaker <57673795+cgdaker@users.noreply.github.com>
2021-03-11 19:49:02 -0500

    saturation changed

--- color/classify_colors.py
+++ color/classify_colors.py
@@ -5,7 +5,7 @@ import numpy as np
 red_lower = np.array([-3,15,15])           # done
 red_upper = np.array([5,255,255])          # done
 pink_lower = np.array([145, 15, 15])
-pink_upper = np.array([155, 235, 235])
+pink_upper = np.array([155, 255, 255])
 green_lower= np.array([70, 15, 15])      # done
 green_upper= np.array([80, 255, 255])    # done
 blue_lower = np.array([115, 15, 15])     # done
