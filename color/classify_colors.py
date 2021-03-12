6ff2499b7bceb755389e26259774e3a6d163c9a7
cgdaker <57673795+cgdaker@users.noreply.github.com>
2021-03-11 19:44:09 -0500

    green

--- color/classify_colors.py
+++ color/classify_colors.py
@@ -6,8 +6,8 @@ red_lower = np.array([-3,15,15])
 red_upper = np.array([5,255,255])
 pink_lower = np.array([295, 15, 15])
 pink_upper = np.array([310, 100, 100])
-green_lower= np.array([55, 15, 15])
-green_upper= np.array([65, 255, 255])
+green_lower= np.array([70, 15, 15])
+green_upper= np.array([80, 255, 255])
 blue_lower = np.array([115, 15, 15])     # done
 blue_upper = np.array([125, 255, 255])   # done
 
