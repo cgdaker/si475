ceb124538258162262d2f45c8820e76fd9baf603
cgdaker <57673795+cgdaker@users.noreply.github.com>
2021-04-12 21:46:26 -0400

    more blue

--- depth/classify_colors.py
+++ depth/classify_colors.py
@@ -11,8 +11,8 @@ pink_lower = np.array([145, 15, 15])       # done
 pink_upper = np.array([155, 255, 255])     # done
 green_lower= np.array([70, 15, 15])        # done
 green_upper= np.array([80, 255, 255])      # done
-blue_lower = np.array([115, 100, 15])       # done
-blue_upper = np.array([125, 255, 255])     # done
+blue_lower = np.array([110, 100, 15])       # done
+blue_upper = np.array([130, 255, 255])     # done
 yellow_lower = np.array([25, 15, 15])       # done
 yellow_upper = np.array([35, 255, 255])     # done
 
