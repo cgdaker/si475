b4ee0d546b5cb63ce679459f34c4f05ee33544b6
cgdaker <57673795+cgdaker@users.noreply.github.com>
2021-04-27 15:19:26 -0400

    beginning

--- final/final.py
+++ final/final.py
@@ -1,3 +1,30 @@
 from state import State
 
-s = State((1, 2), {"B": (12, 3)})
+def get_nodelist(path):
+
+    
+    #open file and iterate
+    file = open(path, 'r')
+    next(file)
+    for line in file:
+        # check for the newline
+        if line == '\n':
+            break
+
+        print(line)
+        # read in the vertex
+        line_parts = line.split('[')
+        label = line_parts[0].strip(' ')
+        coords = line_parts[1].split('"')[1].split(",")
+        x = coords[0].strip('(')
+        y = coords[1].strip(')')
+
+        # create the vertex and add if not already in
+        v = Vertex(label, x, y)
+        adj_matrix[label] = v
+
+# start and end state, nodelist
+path = 'file.txt'
+nodelist = get_nodelist(path)
+init_state = State((0,0), {"B": (1,1) }, nodelist)
+end = State((0,0) {"B": (2,2)}, nodelist)
--- final/state.py
+++ final/state.py
@@ -1,8 +1,15 @@
 class State:
 
     # takes in a position and a disctionary of balloon locations
-    def __init__(self, position, balloons):
+    def __init__(self, position, balloons, nodelist):
         self.position = position
         self.balloons = balloons
+        self.nodes = nodelist
+        
+    # returns list of all possible states
+    def get_possible_states(self):
 
-    
+        # list to return
+        to_return = []
+
+        # find all other positions can go to
